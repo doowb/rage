9301b26f39c9446fb7c77968e7489a701ac1e409
Brian Woodward <brian.woodward@gmail.com>
2015-05-12 13:38:35 -0400

    init setup

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * rage <https://github.com/doowb/rage>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
